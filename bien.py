fb93bcb62a1c33ef8e2136301d21c108a0222d38
Tran Ngoc <ngoc44379@gmail.com>
2024-01-17 21:28:16 +0700

    noi dung

---
+++ bien.py
@@ -0,0 +1,6 @@
+ten_nhom = "C0124L1"
+so_thanh_vien = 7
+da_co_gia_dinh = False
+tong_can_nang = 180
+# Cu phap la : Ten bien = gia tri
+
---
+++ toan-tu-logic.py
@@ -0,0 +1,5 @@
+# and => so sanh 2 ve (tat ca deu dung) -> true
+
+# and => so sanh 2 ve (1 ve sai) -> false
+# or => so sanh 2 ve (1 ve dung) -> true
+# or
