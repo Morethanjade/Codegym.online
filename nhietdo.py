d99383d285deda523728d988377afb627b678e3e
Tran Ngoc <ngoc44379@gmail.com>
2024-02-15 21:13:00 +0700

    Add files via upload

---
+++ nhietdo.py
@@ -0,0 +1,8 @@
+#Nhập vào giá trị độ °C muốn chuyển đổi
+#Tính độ °F tương ứng qua công thức:
+#°F  =  ( °C × 1.8 ) +  32
+#Hiển thị độ °F (In ra màn hình)
+
+C= float(input("Nhap vao nhiet do C :"))
+F = (C*1.8) +32
+print("Nhiet do", C, "do C:", F, "do F.")
